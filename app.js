0dc8baac45dfa1d53a3d9b7f22fdfad7e7474be1
Wagner Sartori <wssartori@gmail.com>
2023-02-08 09:50:33 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o Sistema de Gerenciamento de Jogos").
 console.log("Inciando acesso ao banco de dados").
 // Comentario de Alteração 
 console.log("Nova mensagem para validação de dados").
+console.log("ENviando para o Github").
