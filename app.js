5a9b7cebb693d6f1aad13d258b7b23dc0cc93c5e
Wagner Sartori <wssartori@gmail.com>
2023-02-08 10:31:00 -0300

    Voltando a versao do BladeBR

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o Sistema de Gerenciamento de Jogos").
 console.log("Inciando acesso ao banco de dados").
 // Comentario de Alteração 
 console.log("Nova mensagem para validação de dados").
-console.log("ENviando para o Github").
